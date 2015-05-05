b7bc435fa053901ed8e34ccd6e5f0976790bdc41
Erick Solarte <ersolarte@umariana.edu.co>
2015-05-05 17:33:06 -0500

    adicion de iconos

--- src/java/org/primefaces/showcase/view/overlay/UserLoginView.java
+++ src/java/org/primefaces/showcase/view/overlay/UserLoginView.java
@@ -37,7 +37,7 @@ public class UserLoginView {
          
         if(username != null && username.equals("admin") && password != null && password.equals("admin")) {
             loggedIn = true;
-            message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Bienvenido a iniciado sesion corectamente: ", username);
+            message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Bienvenido a iniciado sesion corectamente: "+ username, username);
         } else {
             loggedIn = false;
             message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Error al iniciar sesion", "usuario y contraseña invalidos");
